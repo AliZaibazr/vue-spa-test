a6e7f8f9d6ee9a2c574ec811bd4d69c3974a8153
Abdul majeed <abdul4majeed@gmail.com>
2021-02-19 21:45:37 +0500

    form handling front end

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue } from 'bootstrap-vue'
 
 Vue.config.productionTip = false;
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+import Vuelidate from 'vuelidate'
+Vue.use(Vuelidate)
+
 
 new Vue({
   router,
---
+++ src/views/Form.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="holder">
+    <Form></Form>
+  </div>
+</template>
+<script>
+import Form from "@/components/Form.vue";
+export default {
+  name: "Forms",
+  components: {
+    Form
+    }
+}
+</script>
+
+<style>
+.holder
+{
+    border:1px solid red;
+    width: 50%;
+    margin: auto;
+}
+</style>
